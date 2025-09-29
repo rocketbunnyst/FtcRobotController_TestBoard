43c93f0f0dd9e93a3af7d67ec441bce37c537c5a
rocketbunnyst <rocketbunnyst@gmail.com>
2025-09-29 09:37:10 -0400

    Added comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learnjavaforFTC/ProgrammingBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learnjavaforFTC/ProgrammingBoard.java
@@ -6,12 +6,18 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class ProgrammingBoard {
+
+    // Declare variables and motors/sensors
     private DigitalChannel touchSensor;
     private DcMotor motor;
     private double ticksPerRotation;
     private Servo servo;
 
     public void init(HardwareMap hwMap) {
+
+        // Initialize motors/sensors (tell the program what named port to look for on the robot)
+        // and give variables their initial values
+
         touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
 
@@ -22,18 +28,24 @@ public class ProgrammingBoard {
         servo = hwMap.get(Servo.class, "servo");
     }
 
+    // Define Additional Methods (other than the native ones) that can be called by your OpModes
+
+    // Finds out if the touch sensor is pressed
     public boolean isTouchSensorPressed() {
         return !touchSensor.getState();
     }
 
+    // Sets the speed of the motor to the number passed
     public void setMotorSpeed(double speed) {
         motor.setPower(speed);
     }
 
+    // Returns the number of times the motor has fully rotated
     public double getMotorRotations() {
         return motor.getCurrentPosition() / ticksPerRotation;
     }
 
+    // Sets the servo position to the number passed
     public void setServoPosition(double position){
         servo.setPosition(position);
     }
