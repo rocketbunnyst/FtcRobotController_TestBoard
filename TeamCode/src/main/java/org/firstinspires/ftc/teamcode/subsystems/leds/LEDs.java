3aa26d531ac3a6fa98cab4a3660bc03cb19b4c1f
rocke <rocketbunnyst@gmail.com>
2025-11-14 13:12:53 -0500

    led modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/leds/LEDs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/leds/LEDs.java
@@ -13,12 +13,22 @@ public class LEDs implements LedsInterface{
 
     private static final Map<LedMode, RevBlinkinLedDriver.BlinkinPattern> modeLookup = new HashMap<>();
 
-    // TEAM_COLORS, ALLIANCE_RED, ALLIANCE_BLUE, INTAKING_FRONT, INTAKING_REAR, INTAKING_THRU,
+    // TEAM_COLORS, ALLIANCE_RED, ALLIANCE_BLUE,INTAKING_FRONT, INTAKING_REAR,INTAKING_THRU,
     // MOTIF_DETECTED, LAUNCHER_ATSPEED, GREEN_NEXT, PURPLE_NEXT, ELEVATING, BLACK
 
     static {
         modeLookup.put(LedMode.TEAM_COLORS, RevBlinkinLedDriver.BlinkinPattern.RAINBOW_FOREST_PALETTE);
         modeLookup.put(LedMode.ALLIANCE_RED, RevBlinkinLedDriver.BlinkinPattern.BREATH_RED);
+        modeLookup.put(LedMode.ALLIANCE_BLUE, RevBlinkinLedDriver.BlinkinPattern.BREATH_BLUE);
+        modeLookup.put(LedMode.INTAKING_FRONT, RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_LAVA_PALETTE );
+        modeLookup.put(LedMode.INTAKING_REAR, RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_FOREST_PALETTE);
+        modeLookup.put(LedMode.INTAKING_THRU, RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_RAINBOW_PALETTE);
+        modeLookup.put(LedMode.GREEN_NEXT, RevBlinkinLedDriver.BlinkinPattern.DARK_GREEN);
+        modeLookup.put(LedMode.PURPLE_NEXT, RevBlinkinLedDriver.BlinkinPattern.VIOLET);
+        modeLookup.put(LedMode.BLACK, RevBlinkinLedDriver.BlinkinPattern.BLACK);
+        modeLookup.put(LedMode.ELEVATING, RevBlinkinLedDriver.BlinkinPattern.STROBE_GOLD);
+        modeLookup.put(LedMode.LAUNCHER_ATSPEED, RevBlinkinLedDriver.BlinkinPattern.RAINBOW_RAINBOW_PALETTE);
+        modeLookup.put(LedMode.MOTIF_DETECTED, RevBlinkinLedDriver.BlinkinPattern.CP1_2_TWINKLES);
     }
 
     public LEDs(HardwareMap hardwareMap) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/LedTestTeleop.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.utilities;
+
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.leds.LEDs;
+import org.firstinspires.ftc.teamcode.subsystems.leds.LedsInterface;
+import org.firstinspires.ftc.teamcode.utilities.SampleCommandTeleop;
+
+@TeleOp
+public class LedTestTeleop extends SampleCommandTeleop {
+    private LEDs leds;
+
+    @Override
+    public void onInit() {
+        leds = new LEDs(hardwareMap);
+    }
+
+    @Override
+    public void onStart() {
+    g1.getGamepadButton(GamepadKeys.Button.X).whenPressed(() -> leds.setMode(LedsInterface.LedMode.TEAM_COLORS));
+    g1.getGamepadButton(GamepadKeys.Button.Y).whenPressed(() -> leds.setMode(LedsInterface.LedMode.ALLIANCE_BLUE));
+    g1.getGamepadButton(GamepadKeys.Button.A).whenPressed(() -> leds.setMode(LedsInterface.LedMode.INTAKING_FRONT));
+    g1.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenPressed(() -> leds.setMode(LedsInterface.LedMode.ELEVATING));
+    g1.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whenPressed(() -> leds.setMode(LedsInterface.LedMode.LAUNCHER_ATSPEED));
+    }
+
+    @Override
+    public void onLoop() {
+        telemetry.addLine("LED CONTROLS");
+        telemetry.addLine("Press X for team colors");
+        telemetry.addLine("Press Y for alliance blue");
+        telemetry.addLine("Press A for intaking front");
+        telemetry.addLine("Press the right bumper for elevating");
+        telemetry.addLine("Press the left bumper for launcher at speed");
+    }
+    @Override
+    public void onStop() {}
+}
